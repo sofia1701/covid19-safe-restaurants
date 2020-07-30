92a1d77af5e99a38b9870b455a788b86a2806f2c
Sofia <sofia.dionisio94@gmail.com>
2020-07-30 12:24:18 +0100

    Added Google fonts to be used in project, installed Airbnb style guide and Prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">Safe Restaurants</div>;
 }
 
 export default App;
