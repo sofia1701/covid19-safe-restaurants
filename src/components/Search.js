cd1bb067070993e227cdff4a0c6916e68281df8a
Sofia <sofia.dionisio94@gmail.com>
2020-08-11 18:29:30 +0100

    Added styling to search box, including serach input and select fields

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,49 +14,37 @@ export default function Search() {
         <h2>Search for a safe restaurant in Manchester</h2>
 
         <form className="search-box">
+          <button className="search-button" type="submit">
+            <FaSearch />
+          </button>
           <input
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             className="search-input"
             type="text"
-            placeholder="e.g. The Bay Horse Tavern..."
+            placeholder="The Bay Horse Tavern..."
           />
-          <button type="submit">
-            <FaSearch />
-          </button>
         </form>
-        <p>Filter by type</p>
-        <div className="categories">
-          <div className="column">
-            <Link to={`/?query={"type": "Pub"}`}>Pub</Link>
-            <Link to={`/?query={"type": "Bistro"}`}>Bistro</Link>
-            <Link to={`/?query={"type": "Kebab"}`}>Kebab</Link>
-            <Link to={`/?query={"type": "African"}`}>African</Link>
-          </div>
-          <div className="column">
-            <Link to={`/?query={"type": "British"}`}>British</Link>
-            <Link to={`/?query={"type": "Mexican"}`}>Mexican</Link>
-            <Link to={`/?query={"type": "Brazilian"}`}>Brazilian</Link>
-            <Link to={`/?query={"type": "Irish"}`}>Irish</Link>
-          </div>
-          <div className="column">
-            <Link to={`/?query={"type": "Italian"}`}>Italian</Link>
-            <Link to={`/?query={"type": "Spanish"}`}>Spanish</Link>
-            <Link to={`/?query={"type": "French"}`}>French</Link>
-            <Link to={`/?query={"type": "Vietnamese"}`}>Vietnamese</Link>
-          </div>
-          <div className="column">
-            <Link to={`/?query={"type": "Vegan"}`}>Vegan</Link>
-            <Link to={`/?query={"type": "Cafe"}`}>Cafe</Link>
-            <Link to={`/?query={"type": "Bakery"}`}>Bakery</Link>
-            <Link to={`/?query={"type": "International"}`}>International</Link>
-          </div>
-          <div className="column">
-            <Link to={`/?query={"type": "Asian"}`}>Asian</Link>
-            <Link to={`/?query={"type": "American"}`}>American</Link>
-            <Link to={`/?query={"type": "Indian"}`}>Indian</Link>
-            <Link to={`/?query={"type": "South African"}`}>South African</Link>
-          </div>
+        <h3>Or select your favourite cuisine</h3>
+
+        <div className="row">
+          <Link to="/">All Restaurants</Link>
+          <Link to={`/?query={"type": "African"}`}>African</Link>
+          <Link to={`/?query={"type": "Asian"}`}>Asian</Link>
+          <Link to={`/?query={"type": "American"}`}>American</Link>
+          <Link to={`/?query={"type": "British"}`}>British</Link>
+          <Link to={`/?query={"type": "Mexican"}`}>Mexican</Link>
+          <Link to={`/?query={"type": "Bakery"}`}>Bakery</Link>
+        </div>
+        <div className="row">
+          <Link to={`/?query={"type": "Brazilian"}`}>Brazilian</Link>
+          <Link to={`/?query={"type": "Italian"}`}>Italian</Link>
+          <Link to={`/?query={"type": "Spanish"}`}>Spanish</Link>
+          <Link to={`/?query={"type": "French"}`}>French</Link>
+          <Link to={`/?query={"type": "Irish"}`}>Irish</Link>
+          <Link to={`/?query={"type": "Vegan"}`}>Vegan</Link>
+          <Link to={`/?query={"type": "International"}`}>International</Link>
+          <Link to={`/?query={"type": "Cafe"}`}>Cafe</Link>
         </div>
       </div>
     </div>
