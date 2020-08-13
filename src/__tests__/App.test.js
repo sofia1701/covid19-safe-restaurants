5978c199c2796cc36923686e298418688e4ac878
Sofia <sofia.dionisio94@gmail.com>
2020-08-13 16:59:34 +0100

    Added basic test for App.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "../components/App";
 
 test("renders the app and all its components correctly ", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
+  const { asFragment } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const component = asFragment();
+
+  expect(component).toMatchSnapshot();
 });
